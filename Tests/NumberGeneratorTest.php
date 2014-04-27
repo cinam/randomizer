322590f10be11a5cad533b0e16e278356e07790b
cinam <cinam@hotmail.com>
2014-04-27 22:26:58 +0200

    Added some tests for Number Generator.

---
+++ Tests/NumberGeneratorTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Randomizer\Tests;
+
+use Randomizer\NumberGenerator;
+use Randomizer\NumberGeneratorInterface;
+use Randomizer\Exception\InvalidArgumentException;
+
+class NumberGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var NumberGeneratorInterface
+     */
+    protected $generator;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->generator = new NumberGenerator();
+    }
+
+    public function tearDown()
+    {
+        $this->generator = null;
+    }
+
+    public function testGetInt()
+    {
+        $this->assertEquals(3, $this->generator->getInt(3, 3));
+        $this->assertEquals(-3, $this->generator->getInt(-3, -3));
+
+        for ($i = 0; $i < 100; $i++) {
+            $min = \mt_rand(-1000, 1000);
+            $max = \mt_rand($min, $min + 2000);
+
+            $randomNumber = $this->generator->getInt($min, $max);
+            $this->assertGreaterThanOrEqual($min, $randomNumber);
+            $this->assertLessThanOrEqual($max, $randomNumber);
+        }
+    }
+
+    public function testGetFloat()
+    {
+        $this->assertEquals(3.5, $this->generator->getFloat(3.5, 3.5));
+        $this->assertEquals(-3.5, $this->generator->getFloat(-3.5, -3.5));
+
+        for ($i = 0; $i < 100; $i++) {
+            $min = \mt_rand(-1000, 1000) + (\mt_rand(0, 100) / 100);
+            $max = $min + (\mt_rand(0, 100) / 100);
+
+            $randomNumber = $this->generator->getFloat($min, $max);
+            $this->assertGreaterThanOrEqual($min, $randomNumber);
+            $this->assertLessThanOrEqual($max, $randomNumber);
+        }
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testIfGetIntThrowsExceptions()
+    {
+        $this->generator->getInt(5, 4);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testIfGetFloatThrowsExceptions()
+    {
+        $this->generator->getFloat(5.0002, 5.0001);
+    }
+}
