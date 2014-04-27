254f6b88a1621de251106c87f58fe1d755e3fbf9
cinam <cinam@hotmail.com>
2014-04-27 21:18:12 +0200

    Added interface for number generator.

---
+++ NumberGeneratorInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Randomizer;
+
+/**
+ * Interface for number generator.
+ *
+ * @author cinam <cinam@hotmail.com>
+ */
+interface NumberGeneratorInterface
+{
+    /**
+     * Returns random integer from given range.
+     *
+     * @param integer $min The minimal value.
+     * @param integer $max The maximal value.
+     *
+     * @return integer
+     *
+     * @throws InvalidArgumentException If min is greater than max.
+     */
+    public function getInt($min, $max);
+
+    /**
+     * Returns random float from given range.
+     *
+     * @param float $min The minimal value.
+     * @param float $max The maximal value.
+     *
+     * @return float
+     *
+     * @throws InvalidArgumentException If min is greater than max.
+     */
+    public function getFloat($min, $max);
+}
